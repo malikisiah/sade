9b8af355e5c0d6b21e47bb199b5eda796f3d4a48
Malik Teague <malikisiah214@gmail.com>
2023-05-26 17:28:40 -0700

    added several pages

---
+++ src/app/dashboard/Gallery/page.tsx
@@ -0,0 +1,7 @@
+export default function GalleryPage() {
+  return (
+    <div className='min-h-screen flex align-middle items-center justify-center'>
+      <h1 className='text-center text-xl font-black'> Gallery Page</h1>
+    </div>
+  );
+}
--- src/app/dashboard/navBar.tsx
+++ src/app/dashboard/navBar.tsx
@@ -9,6 +9,9 @@ export default function Navbar(props: navbarProps) {
   return (
     <div className='navbar border-b-2 border-neutral bg-base-100 sticky top-0 z-0 px-5'>
       <div className='flex-1 hidden lg:flex'>
+        <Link className='btn btn-ghost normal-case text-xl' href='/dashboard'>
+          Home
+        </Link>
         <Link
           className='btn btn-ghost normal-case text-xl'
           href='/dashboard/musicPage'
@@ -23,7 +26,7 @@ export default function Navbar(props: navbarProps) {
         </Link>
         <Link
           className='btn btn-ghost normal-case text-xl'
-          href='/dashboard/Biography'
+          href='/dashboard/Bio'
         >
           Biography
         </Link>
@@ -50,13 +53,72 @@ export default function Navbar(props: navbarProps) {
           className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
         >
           <li>
-            <Link href='/dashboard/musicPage'>Music</Link>
+            <Link href='/dashboard'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                className='w-5 h-5'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              Home
+            </Link>
           </li>
           <li>
-            <a>Gallery</a>
+            <Link href='/dashboard/musicPage'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                className='w-5 h-5'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M17.721 1.599a.75.75 0 01.279.584v11.29a2.25 2.25 0 01-1.774 2.198l-2.041.442a2.216 2.216 0 01-.938-4.333l2.662-.576a.75.75 0 00.591-.734V6.112l-8 1.73v7.684a2.25 2.25 0 01-1.774 2.2l-2.042.44a2.216 2.216 0 11-.935-4.33l2.659-.574A.75.75 0 007 12.53V4.237a.75.75 0 01.591-.733l9.5-2.054a.75.75 0 01.63.149z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              Music
+            </Link>
           </li>
           <li>
-            <a>Biography</a>
+            <Link href='/dashboard/Gallery'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                className='w-5 h-5'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M1 8a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 018.07 3h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0016.07 6H17a2 2 0 012 2v7a2 2 0 01-2 2H3a2 2 0 01-2-2V8zm13.5 3a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM10 14a3 3 0 100-6 3 3 0 000 6z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link href='/dashboard/Bio'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                className='w-5 h-5'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M1 6a3 3 0 013-3h12a3 3 0 013 3v8a3 3 0 01-3 3H4a3 3 0 01-3-3V6zm4 1.5a2 2 0 114 0 2 2 0 01-4 0zm2 3a4 4 0 00-3.665 2.395.75.75 0 00.416 1A8.98 8.98 0 007 14.5a8.98 8.98 0 003.249-.604.75.75 0 00.416-1.001A4.001 4.001 0 007 10.5zm5-3.75a.75.75 0 01.75-.75h2.5a.75.75 0 010 1.5h-2.5a.75.75 0 01-.75-.75zm0 6.5a.75.75 0 01.75-.75h2.5a.75.75 0 010 1.5h-2.5a.75.75 0 01-.75-.75zm.75-4a.75.75 0 000 1.5h2.5a.75.75 0 000-1.5h-2.5z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              Biography
+            </Link>
           </li>
         </ul>
       </div>
